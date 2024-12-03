a73e8b38e7cfae6fbe8724f98b7553bb03e2b2c5
mehedi20695 <mehedi.m000@gmail.com>
2024-12-03 23:39:39 +0600

    Setup routes on the project

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className='text-5xl text-orange-700 font-bold text-center'>Crowd-Cube</h1>
-    </>
-  )
-}
-
-export default App
---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h2>Hello from Crowd-cube</h2>
+    }
+]);
+
+export default router;

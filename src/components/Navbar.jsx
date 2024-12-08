5cafc49c1325012d3e5e90a0b428508aa4a43cfb
mehedi20695 <mehedi.m000@gmail.com>
2024-12-08 16:40:41 +0600

    Found some bug and resolve them and commit again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content rounded-box z-[1] bg-teal-400 mt-3 w-52 p-2 shadow"
           >
             <Fade cascade>
               <li>
--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -68,7 +68,7 @@ const CampaignDetails = () => {
     return (
         <div className="max-w-3xl mx-auto p-6">
             <Helmet>
-                <title>Campaigns-Details | Crowd-Cube</title>
+                <title>Campaign-Details | Crowd-Cube</title>
             </Helmet>
             <h2 className="text-3xl font-bold mb-6">{campaign.title}</h2>
             <img
